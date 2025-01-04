1135175356d592d4ff14fa3968513e564dd7b89a
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2025-01-04 12:29:53 -0400

    feat: add countBits() to count bits to 1 in binary

--- 6kyu/bit-counting.js
+++ 6kyu/bit-counting.js
@@ -9,3 +9,7 @@ Example: The binary representation of 1234 is 10011010010, so the function shoul
 var countBits = function (n) {
   return n.toString(2).split(1).length - 1;
 };
+
+var countBits = function (n) {
+  return n.toString(2).replace(/0/g, "").length;
+};
