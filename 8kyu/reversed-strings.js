4c1e91e3b2f4aa5bb9b2568af0dc82469667ed33
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2025-01-07 12:35:01 -0400

    feat: add solution() to reverse a string

---
+++ 8kyu/reversed-strings.js
@@ -0,0 +1,12 @@
+/*
+
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+*/
+
+function solution(str) {
+  return [...str].reverse().join("");
+}
